bd9ee402a73ac057ec575a0eb034fd89a10bba1b
Brian Misiak <bmisiak@me.com>
2021-01-22 07:38:51 -0800

    simpler, no async

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ struct Model {
     id: i32
 }
 
-async fn repro(postgres: &sqlx::PgPool) -> Result<Model, sqlx::Error> {
-    let x = sqlx::query_as::<_,Model>("SELECT 1")
+ fn repro(postgres: &sqlx::PgPool) -> () {
+    let _x = sqlx::query_as::<_,Model>("SELECT 1")
     .bind(12) // comment this out
     .fetch_one(postgres);
 
-    x.await
+    ()
 }
 
 fn main() {
